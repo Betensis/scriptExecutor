17f0a4a997d8a2604fe9f1f03680fa7e1cda4c21
betensis <betensis@gmail.com>
2021-10-06 23:24:35 +0200

    2.0!

--- starter.py
+++ starter.py
@@ -1,46 +1,70 @@
 import os
 from pathlib import Path
 import subprocess
-from typing import Generator
+from typing import Generator, Optional, Union
 
 import fire
 from fire.core import FireError
 
 
 class Scripts:
-    executors_by_suffix = {
-        ".sh": "bash",
-        ".py": "python3",
-    }
+    def __init__(self):
+        self.scripts_dir_environment_name = "SCRIPT_DIR"
+        self.executors_by_suffix = {
+            ".sh": "bash",
+            ".py": "python3",
+        }
 
     @property
     def scripts_dir(self):
-        scripts_dir = Path(os.environ.get("SCRIPT_DIR") or "./scripts")
-        if not scripts_dir.exists():
-            raise FireError(f"scripts directory {scripts_dir.resolve()} didn't exists")
-        if scripts_dir.is_file():
-            raise FireError(f"{scripts_dir.resolve()} must be directory not file")
-        return scripts_dir
+        res = self.__get_scripts_dir_path()
+        if res is None:
+            raise FireError(
+                "Invalid scripts directory path. "
+                f"The {self.scripts_dir_environment_name} environment variable may have an incorrect value."
+            )
+        return res
 
-    def _get_all_scripts_files(self) -> Generator[Path, None, None]:
+    def set_scripts_dir(self, dir_path: str) -> None:
+        """Set scripts directory path"""
+        if not self.__is_valid_scripts_dir(dir_path):
+            raise FireError(f"Invalid directory path {dir_path}")
+
+        print(os.environ.setdefault(self.scripts_dir_environment_name, dir_path))
+        print(f"Current script dir: {dir_path}")
+
+    def __get_scripts_dir_path(self) -> Optional[Path]:
+        """Return path if path is valid else return None"""
+        dir_path = os.environ.get(self.scripts_dir_environment_name)
+        if dir_path is None or not self.__is_valid_scripts_dir(dir_path):
+            return None
+        return Path(dir_path)
+
+    @staticmethod
+    def __is_valid_scripts_dir(path: Union[Path, str]) -> bool:
+        if isinstance(path, str):
+            path = Path(path)
+        return path.exists() and path.is_dir()
+
+    def get_all_scripts_files(self) -> Generator[Path, None, None]:
         yield from filter(lambda x: x.is_file(), self.scripts_dir.iterdir())
 
     def all_scripts(self) -> Generator[str, None, None]:
         """Show all scripts"""
-        yield from map(lambda file: file.name, self._get_all_scripts_files())
+        yield from map(lambda file: file.name, self.get_all_scripts_files())
 
-    def execute(self, file: str) -> None:
-        """Execute script by name"""
-        if not file in self.all_scripts():
-            raise FireError(f"file doesn't exist")
+    def execute(self, file: str, executor: str = None) -> None:
+        """Execute script by file name"""
+        if file not in self.all_scripts():
+            raise FireError(f"File: {file} doesn't exist")
 
         full_path = self.scripts_dir.joinpath(file)
-        file_executor = self.executors_by_suffix[full_path.suffix]
-        if not (
-            code := subprocess.run(
-                [f"{file_executor} {full_path.resolve()}"], shell=True
-            )
-        ):
+        if executor:
+            if not (
+                code := subprocess.run([f"{executor} {full_path.resolve()}"], shell=True)
+            ):
+                raise FireError(f"script exit with code {code}")
+        if not (code := subprocess.run([f".{full_path.resolve()}"], shell=True)):
             raise FireError(f"script exit with code {code}")
 
 
@@ -48,11 +72,15 @@ class Starter:
     def __init__(self):
         self.scripts = Scripts()
 
-    def start_all(self):
+    def start_all(self) -> None:
         """Execute all scripts"""
-        for script in self.scripts._get_all_scripts_files():
+        for script in self.scripts.get_all_scripts_files():
             print(f"Try execute {script.name}")
             self.scripts.execute(script.name)
+
+    def execute(self, file: str, executor: str = None) -> None:
+        """Execute script by file name"""
+        self.scripts.execute(file, executor)
 
 
 if __name__ == "__main__":
