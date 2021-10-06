8b95589e644a2938b3a60655939c949de74ac09b
betensis <betensis@gmail.com>
2021-10-06 23:52:02 +0200

    2.0! again

--- starter.py
+++ starter.py
@@ -9,11 +9,7 @@ from fire.core import FireError
 
 class Scripts:
     def __init__(self):
-        self.scripts_dir_environment_name = "SCRIPT_DIR"
-        self.executors_by_suffix = {
-            ".sh": "bash",
-            ".py": "python3",
-        }
+        self.scripts_dir_environment_name = "SCRIPTS_DIR"
 
     @property
     def scripts_dir(self):
@@ -24,14 +20,6 @@ class Scripts:
                 f"The {self.scripts_dir_environment_name} environment variable may have an incorrect value."
             )
         return res
-
-    def set_scripts_dir(self, dir_path: str) -> None:
-        """Set scripts directory path"""
-        if not self.__is_valid_scripts_dir(dir_path):
-            raise FireError(f"Invalid directory path {dir_path}")
-
-        print(os.environ.setdefault(self.scripts_dir_environment_name, dir_path))
-        print(f"Current script dir: {dir_path}")
 
     def __get_scripts_dir_path(self) -> Optional[Path]:
         """Return path if path is valid else return None"""
@@ -59,13 +47,18 @@ class Scripts:
             raise FireError(f"File: {file} doesn't exist")
 
         full_path = self.scripts_dir.joinpath(file)
-        if executor:
-            if not (
-                code := subprocess.run([f"{executor} {full_path.resolve()}"], shell=True)
-            ):
+        try:
+            if executor:
+                if not (
+                    code := subprocess.run([f"{executor} {full_path.resolve()}"])
+                ):
+                    raise FireError(f"script exit with code {code}")
+            if not (code := subprocess.run([f"{full_path.resolve()}"])):
                 raise FireError(f"script exit with code {code}")
-        if not (code := subprocess.run([f".{full_path.resolve()}"], shell=True)):
-            raise FireError(f"script exit with code {code}")
+        except OSError as e:
+            raise FireError(
+                f"Can't execute {file}. {e.strerror}"
+            )
 
 
 class Starter:
@@ -75,7 +68,7 @@ class Starter:
     def start_all(self) -> None:
         """Execute all scripts"""
         for script in self.scripts.get_all_scripts_files():
-            print(f"Try execute {script.name}")
+            print(f"------Try execute {script.name}------")
             self.scripts.execute(script.name)
 
     def execute(self, file: str, executor: str = None) -> None:
